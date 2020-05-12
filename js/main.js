da0792b45087f13384f39cb91cd31f90af8f1f6d
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-05-12 18:57:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ $('.title__btn').on('click',function(){
   $(this).closest('.grid__content-small').next().toggleClass('disabled')
 });
 
+$('.header__btn').on('click',function(){
+  $('.header__navlist-mobile').toggleClass('active-menu'),
+  $(this).toggleClass('btn-close')
+});
+
 
 $('.grid__slider').slick({
   autoplay: true,
